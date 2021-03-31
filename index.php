5c255eec5cf239ba08140f10c140ff8adfd7bfff
AmPtUrYO <britta-exner@gmx.de>
2021-03-31 22:47:22 +0200

    .

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+$title = 'Index';
+require_once 'includes/header.php';
+?>
+
+<h1 class="text-center">Informationsportal Badersleben</h1>
+<p>content</p>
+
+<?php 
+require_once 'includes/footer.php';
+?>
