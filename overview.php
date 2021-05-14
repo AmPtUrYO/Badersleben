744f2845e695694b03428f8e3b1eec307222d6b4
AmPtUrYO <britta-exner@gmx.de>
2021-05-14 22:05:08 +0200

    .

--- overview.php
+++ overview.php
@@ -3,11 +3,9 @@ $title = ucfirst($_GET['id']);
 require_once 'includes/header.php';
 require_once 'includes/sidebar.php';
 $result = DB::run("SELECT * FROM content INNER JOIN sections ON content.section_id=sections.section_id WHERE content.section_id='" . $_GET['id']."' ORDER BY date DESC");
-$header = $result->fetchObject()->section_name;
+$header = DB::run("SELECT * FROM SECTIONS WHERE section_id='" . $_GET['id']."'")->fetchObject();
 ?>
-
-
-<h1 class="text-center text-capitalize"><?php echo $header; ?></h1>
+<h1 class="text-center text-capitalize"><?php echo $header->section_name; ?></h1>
 
 <?php
  foreach($result as $row){?>
